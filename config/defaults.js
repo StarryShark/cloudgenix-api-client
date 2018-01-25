c339ea78a55299c231e23fc474c2dbd0a501a043
Sumit Goel <sumitgoelpw@users.noreply.github.com>
2018-01-24 18:14:19 -0800

    add sites and elements api calls

--- config/defaults.js
+++ config/defaults.js
@@ -4,12 +4,16 @@ module.exports = {
     'login': 'v2.0',
     'profile': 'v2.0',
     'query_events': 'v3.0',
+    'sites': 'v4.1',
+    'elements': 'v2.0',
     'logout': 'v2.0'
   },
   'uriMap': {
     'login': '/%s/api/login',
     'profile': '/%s/api/profile',
     'query_events': '/%s/api/tenants/%s/events/query',
+    'sites': '/%s/api/tenants/%s/sites',
+    'elements': '/%s/api/tenants/%s/elements',
     'logout': '/%s/api/logout'
   }
 };
